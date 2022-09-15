56ffd9ab2d4f06ae53db4fd7e4812b2eb5924a07
worapit <drm.wmwork@gmail.com>
2022-09-15 14:07:44 +0700

    Update1

--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div className="vstack gap-3">
+        <div className="d-flex align-items-center gap-3">
+          <img
+            src={props.image}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <span className="fw-semibold fs-5">
+            {props.name} {props.id}
+          </span>
+        </div>
+        <span>Quiz นี้สอบผ่าน สาธุ #261207</span>
+        <div className="d-flex align-items-center gap-1">
+          <img src="/like.svg" width={20}></img>
+          <span className="text-muted">100 คน</span>
+        </div>
+        <hr className="m-0" />
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Comment from "../components/Comment";
 import PostOwner from "../components/PostOwner";
+import Reply from "../components/Reply";
 import { comments } from "../libs/comments";
 
 export default function Home() {
@@ -13,48 +14,11 @@ export default function Home() {
         style={{ maxWidth: "700px" }}
         className="mx-auto bg-white p-3 rounded rounded-3 shadow-sm"
       >
-        {/* PostOwner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5">Chayanin Suatap 12345678</span>
-          </div>
-          <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span className="text-muted">100 คน</span>
-          </div>
-          <hr className="m-0" />
-        </div>
-
-        {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#E5E7EB" }}
-          >
-            <span className="fw-semibold">Lisa</span>
-            <br />
-            <span>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span className="text-muted">999 คน</span>
-            </div>
-          </div>
-        </div>
+        <PostOwner
+          image="/profileImages/profile.jpg"
+          name="Worapitcha Muangyot"
+          id="640610666"
+        />
 
         {/* Reply Example */}
         <div className="d-flex gap-2 my-2 ps-5">
