3f4352db271964867621d60ac3da9e14f904735d
worapit <drm.wmwork@gmail.com>
2022-09-15 14:49:15 +0700

    Update3

--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { comments } from "../libs/comments";
 
 export default function Reply(props) {
-  return <div></div>;
+  return comments.map((x, i) => (
+    <div className="d-flex gap-2 my-2 ps-5" key={i}>
+      <img
+        src={x.replies.userImagePath}
+        width="48"
+        height="48"
+        className="rounded-circle"
+        style={{ objectFit: "cover" }}
+      />
+      <div
+        className="rounded rounded-3 p-2"
+        style={{ backgroundColor: "#E5E7EB" }}
+      >
+        <span className="fw-semibold">{x.replies.username}</span>
+        <br />
+        <span>{x.replies.replyText}</span>
+        <div className="d-flex align-items-center gap-1">
+          <img src="/like.svg" width={20}></img>
+          <span className="text-muted">{x.replies.likeNum} คน</span>
+        </div>
+      </div>
+    </div>
+  ));
 }
--- pages/index.js
+++ pages/index.js
@@ -20,25 +20,6 @@ export default function Home() {
         />
 
         <Comment />
-
-        {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#E5E7EB" }}
-          >
-            <span className="fw-semibold">หมาน้อย</span>
-            <br />
-            <span>จริงค้าบบบบบบบบ</span>
-          </div>
-        </div>
       </div>
     </div>
   );
